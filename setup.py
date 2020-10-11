672dfbf029b690a5b6abbcdcbee1e05b42529fb3
Tiger Sachse <tiger@sachse.io>
2020-10-11 13:48:09 -0700

    Initial commit of ROS1 code.

---
+++ setup.py
@@ -0,0 +1,9 @@
+"""Setup the ezrassor_keyboard_controller ROS package."""
+import distutils.core
+import catkin_pkg.python_setup
+
+setup_arguments = catkin_pkg.python_setup.generate_distutils_setup(
+    packages=("ezrassor_keyboard_controller",),
+    package_dir={"": "source"},
+)
+distutils.core.setup(**setup_arguments)
---
+++ source/ezrassor_keyboard_controller/__init__.py
@@ -0,0 +1,3 @@
+"""Initialize this package with all public objects."""
+from event_generation import Event, pynput_events
+from event_processing import leader_keys, handle_keypresses
